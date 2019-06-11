585dd549eb5a0e572e085b60dd428c1eb4d4d957
ariflateefshah <arif@ideas2it.com>
2019-06-11 12:56:05 +0530

    Copiled main.ts into main.js and added watcher

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-export {}
-let message = 'Hello World back';
-
+"use strict";
+exports.__esModule = true;
+var message = 'Hello World back';
 console.log(message);
-
-
 // Add node main.ts --watch to add watcher to the main.ts file
 // Whenever you are making any changes in the file
 // It will automatically compiled
